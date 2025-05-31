4ba764771e6848e38965053b76d48d49fb9a9dc3
anitoono45 <anitoono45@gmail.com>
2025-05-31 21:01:15 +0500

    Update posts.jskkzkzh

--- posts.js
+++ posts.js
@@ -1,5 +1,5 @@
 // posts.js
-// This file contains the initial data for anime post.
+// This file contains the initial data for anime posts.
 
 export const initialPosts = [
                                                     { id: 1, title: "Super Mario Bros Movie", content: "click to watch", imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0ZJ6mD8c41yfKCkzMB31PjOc4lQAHsjBtPblEk8rgFZEKA55VOrvPLaB7Xu7n9NQmvq9H4AMSCMTSE4w6K0DYegjEluUG8aXeNgf3DQEtGga8kbesrTn58botILFHXORCDCEIN7Ar8Q1jn0FDNrOw_orbasxl6jyZE1l9BvDJQO9Mnb3Ae0aut25qCAji/s320/images%20(16).jpeg", url: "https://anitoono45.github.io/super-mario-bros/" },
@@ -9,7 +9,7 @@ export const initialPosts = [
             { id: 5, title: "Doraemon Nobita's Sky Utopia", content: "click to watch.", imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXN_8lHhbcggDCqkeLRW6McMGFaFzZe1C6E-Z38rF2ceSIh-QV3-lOc-FUETvXkvQJ7p3JxG4EaejdRNsU93Ww7IYUaTVgFqFDKKhyphenhyphenvVGzJrDEHZpnH05aKy78Aef2Cz3zV3asGhq2XPz3I8idv4Hb8aeOEzT7JVT3WOznPt9zKGEew2HVFNesFMvojV2p/s659/images%20(15).jpeg",url: "https://anitoono45.github.io/Doraemon-the-Movie-Nobita-s-Sky-Utopia/" },
             { id: 6, title: "Doraemon nobita chala chand pe", content: "click to watch.", imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIXxXfwIMQXGCQyas0Pm3nSxS7zGQoe-q07mMwhW65mEi7P6zn8CwDzHTFB95n0W9vJzgh0c8OjRUHo-eU9t4E9jXBUksKaeK3iWCXcPbaovbalb81wfecqR_0kVYbqJjhdpiJhFxPk-Aama5spIRyil9vwwwQUJByf3zWMyImgJbreKyNWTPorYO93ScP/s565/images%20(16).jpeg", url: "https://anitoono45.github.io/Doraemon-nobita-chala-chand-pe/" },
             { id: 7, title: "Doraemon Nobita Chal Pada Antarctica", content: "click to watch.", imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjJZc59zZupUl4tMR5IQXXgZ5AQQLDCf_Xxh6IfHSQWrG69ZXfmLAKiNQ6ymGmsCNfN7FoMmQ4e-dxGQtul3JrB79iCjuSJ5hWJhUr0Lui2Z-vJ8sYtr-_xBCpe29d-VTitaT7uLfHrbMdlbd82A1RLZ2q4ZRnsAXR4-4iyZ1Kqo27X1yidgQvvergX7fns/s375/Doraemon_movie_2017.jpeg", url: "https://anitoono45.github.io/Doraemon-Nobita-Chal-Pada-Antarctica/" },
-            { id: 8, title: "Anime Title 8", content: "Anime description 8.", imageUrl: "https://placehold.co/400x550/EEE/31343C?text=Poster+8", url: "watch.html?id=8" },
+            { id: 8, title: "Doraemon Nobita’s Treasure Island", content: "click to watch.", imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQk80C7eMgS1D_t5Xpyb2GJQ3w2hMu0D_pUK-5jg3IErVYcNbIpseptgKVrYeSDbuPAY3H72rEH1cqBAUZ54__rK3CHvLjRu7iyc0vnZmN7gEundrFdK8D5gKvosEfUa2p7DI2WwziCwhyphenhyphenMLLAYrf30Z39R_X7rVrumlcpnuZkZT6IYBciuT6UWAfRAvoT/s512/unnamed.jpg", url: "https://anitoono45.github.io/Doraemon-Nobita-s-Treasure-Island/" },
             { id: 9, title: "Anime Title 9", content: "Anime description 9.", imageUrl: "https://placehold.co/400x550/EEE/31343C?text=Poster+9", url: "watch.html?id=9" },
             { id: 10, title: "Anime Title 10", content: "Anime description 10.", imageUrl: "https://placehold.co/400x550/EEE/31343C?text=Poster+10", url: "watch.html?id=10" },
     // Add your new posts here, like this:
